477f78af776aa0cfb5a0457994b20a93b7e7ddcb
MB <michalbaczyk1995@interia.pl>
2022-06-30 12:26:22 +0200

    pisanie Adam

---
+++ src/main/java/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    String name;
+    String two;
+    public hello(String name, String two) {
+        this.name = name;
+        this.two = two;
+    }
+    public static String helloMethod (String s,String p){
+        return  "Hello" + s + " your number: " + p;
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello Git second user");
+        String a = "adam";
+        hello hello = new hello("Adam", "dwa");
+        System.out.println(hello.helloMethod("adam","two"));
+        
     }
 
-    private String hello(String s) {
-        return s + "Hello";
-    }
 }
