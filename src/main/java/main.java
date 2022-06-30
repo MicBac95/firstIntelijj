bd15cb3c25bf5550f3934ae2a2718013f0a9b9f4
MB <michalbaczyk1995@interia.pl>
2022-06-30 11:56:58 +0200

    zmiana bialego repo

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,4 +2,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello Git second user");
     }
+
+    private String hello(String s) {
+        return s + "Hello";
+    }
 }
